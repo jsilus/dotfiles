6f9616b6de9d8090ec71fbd577cd88991949f5bc
Justin Silus <justin.silus@gmail.com>
2024-04-04 01:53:27 -0500

    aligned menu

--- nvim/.config/nvim/lua/jsilus/plugins/alpha.lua
+++ nvim/.config/nvim/lua/jsilus/plugins/alpha.lua
@@ -21,7 +21,7 @@ return {
             dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
             dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
             dashboard.button("SPC fg", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-            dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+            dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
         }
 
         alpha.setup(dashboard.opts)
