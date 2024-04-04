abaee90e0ba165fe3e1639d64d908c6e4a3c9f31
Justin Silus <justin.silus@gmail.com>
2024-04-04 00:26:15 -0500

    git keybind rename and which-key groups

--- nvim/.config/nvim/lua/jsilus/plugins/which-key.lua
+++ nvim/.config/nvim/lua/jsilus/plugins/which-key.lua
@@ -5,5 +5,19 @@ return {
         vim.o.timeout = true
         vim.o.timeoutlen = 300
     end,
-    config = true,
+    config = function()
+        local wk = require("which-key")
+
+        wk.register({ ["<leader>"] = {
+            b = { name = "buffers" },
+            e = { name = "explorer" },
+            f = { name = "find file" },
+            h = { name = "git" },
+            s = { name = "split window" },
+            x = { name = "error window" },
+        }})
+
+
+        wk.setup()
+    end,
 }
